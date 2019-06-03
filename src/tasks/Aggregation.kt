4c26794fab4aa3f32d10bcf151ed219b9e9d9a79
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-06-03 11:26:11 +0200

    Improved comment

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -2,11 +2,17 @@ package tasks
 
 import contributors.User
 
-// TODO: Write aggregation code.
-//  In the initial list each user is present several times, once for each
-//  repository he or she contributed to.
-//  Merge duplications: each user should be present only once in the resulting list
-//  with the total value of contributions for all the repositories.
-//  Users should be sorted in a descending order by their contributions.
+/*
+TODO: Write aggregation code.
+
+ In the initial list each user is present several times, once for each
+ repository he or she contributed to.
+ Merge duplications: each user should be present only once in the resulting list
+ with the total value of contributions for all the repositories.
+ Users should be sorted in a descending order by their contributions.
+
+ The corresponding test can be found in test/tasks/AggregationKtTest.kt.
+ You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
+*/
 fun List<User>.aggregate(): List<User> =
     this
