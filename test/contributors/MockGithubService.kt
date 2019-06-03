c242b8ecaa5a10274003466d61cdd3d14c7785bd
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2019-06-03 18:09:26 +0200

    Removed rx methods from GitHubService

--- test/contributors/MockGithubService.kt
+++ test/contributors/MockGithubService.kt
@@ -1,6 +1,5 @@
 package contributors
 
-import io.reactivex.Observable
 import kotlinx.coroutines.delay
 import retrofit2.Call
 import retrofit2.Response
@@ -24,13 +23,5 @@ object MockGithubService : GitHubService {
         val testRepo = reposMap.getValue(repo)
         delay(testRepo.delay)
         return Response.success(testRepo.users)
-    }
-
-    override fun getOrgReposRx(org: String): Observable<Response<List<Repo>>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getRepoContributorsRx(owner: String, repo: String): Observable<Response<List<User>>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
