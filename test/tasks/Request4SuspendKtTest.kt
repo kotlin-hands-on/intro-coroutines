7ccea076dba60678539228001620a72293051e3b
Sangyeong Chung <voidreamy@gmail.com>
2019-09-01 17:38:24 +0900

    Task 8: refactored tests to use virtual time

--- test/tasks/Request4SuspendKtTest.kt
+++ test/tasks/Request4SuspendKtTest.kt
@@ -3,25 +3,24 @@ package tasks
 import contributors.MockGithubService
 import contributors.expectedResults
 import contributors.testRequestData
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
 import org.junit.Test
 
 class Request4SuspendKtTest {
+    @ExperimentalCoroutinesApi
     @Test
-    fun testSuspend() = runBlocking {
-        val startTime = System.currentTimeMillis()
+    fun testSuspend() = runBlockingTest {
+        val startTime = currentTime
         val result = loadContributorsSuspend(MockGithubService, testRequestData)
         Assert.assertEquals("Wrong result for 'loadContributorsSuspend'", expectedResults.users, result)
-        val totalTime = System.currentTimeMillis() - startTime
-        /*
-        // TODO: uncomment this assertion
+        val totalTime = currentTime - startTime
         Assert.assertEquals(
             "The calls run consequently, so the total virtual time should be 4000 ms: " +
                     "1000 for repos request plus (1000 + 1200 + 800) = 3000 for sequential contributors requests)",
             expectedResults.timeFromStart, totalTime
         )
-        */
         Assert.assertTrue(
             "The calls run consequently, so the total time should be around 4000 ms: " +
                     "1000 for repos request plus (1000 + 1200 + 800) = 3000 for sequential contributors requests)",
--- test/tasks/Request6ProgressKtTest.kt
+++ test/tasks/Request6ProgressKtTest.kt
@@ -3,24 +3,23 @@ package tasks
 import contributors.MockGithubService
 import contributors.progressResults
 import contributors.testRequestData
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
 import org.junit.Test
 
 class Request6ProgressKtTest {
+    @ExperimentalCoroutinesApi
     @Test
-    fun testProgress() = runBlocking {
-        val startTime = System.currentTimeMillis()
+    fun testProgress() = runBlockingTest {
+        val startTime = currentTime
         var index = 0
         loadContributorsProgress(MockGithubService, testRequestData) {
             users, _ ->
             val expected = progressResults[index++]
-            val time = System.currentTimeMillis() - startTime
-            /*
-            // TODO: uncomment this assertion
+            val time = currentTime - startTime
             Assert.assertEquals("Expected intermediate result after virtual ${expected.timeFromStart} ms:",
                 expected.timeFromStart, time)
-            */
             Assert.assertEquals("Wrong intermediate result after $time:", expected.users, users)
         }
     }
