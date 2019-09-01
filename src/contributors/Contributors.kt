867e44cf53fb91afa776dc9de393464dd03f8d64
Sangyeong Chung <voidreamy@gmail.com>
2019-09-01 14:53:29 +0900

    used exitProcess()

--- src/contributors/Contributors.kt
+++ src/contributors/Contributors.kt
@@ -8,6 +8,7 @@ import tasks.*
 import java.awt.event.ActionListener
 import javax.swing.SwingUtilities
 import kotlin.coroutines.CoroutineContext
+import kotlin.system.exitProcess
 
 enum class Variant {
     BLOCKING,         // Request1Blocking
@@ -38,7 +39,7 @@ interface Contributors: CoroutineScope {
         addOnWindowClosingListener {
             job.cancel()
             saveParams()
-            System.exit(0)
+            exitProcess(0)
         }
 
         // Load stored params (user & password values)
