57f2c030b55717b902fa7644694a3d445135ce9a
Sangyeong Chung <voidreamy@gmail.com>
2019-09-01 17:23:10 +0900

    Task 7: implemented loadContributorsProgress()

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -1,6 +1,9 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -11,6 +14,26 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: listOf()
+
+        val channel = Channel<List<User>>()
+        repos.forEach { repo ->
+            launch {
+                val users = service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(users)
+            }
+        }
+
+        var allUsers = emptyList<User>()
+        repeat(repos.size) {
+            allUsers = (allUsers + channel.receive()).aggregate()
+            updateResults(allUsers, it == repos.lastIndex)
+        }
     }
 }
