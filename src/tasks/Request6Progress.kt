c5e644641e8a5235be0587ad6e07bec309983cd0
Sangyeong Chung <voidreamy@gmail.com>
2019-09-01 16:58:13 +0900

    Task 6: implemented loadContributorsProgress()

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -7,5 +7,19 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    var allUsers = emptyList<User>()
+    repos.forEachIndexed { index, repo ->
+        val users = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        allUsers = (allUsers + users).aggregate()
+        updateResults(allUsers, index == repos.lastIndex)
+    }
 }
