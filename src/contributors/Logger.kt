436a95aff85064bdbe004b04a4e62948527d68c2
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2020-03-07 20:00:15 +0300

    Delete manual loggers
    Built-in ktor logger does the job

--- src/contributors/Logger.kt
+++ src/contributors/Logger.kt
@@ -9,23 +9,3 @@ val log: Logger = LoggerFactory.getLogger("Contributors")
 fun log(msg: String?) {
     log.info(msg)
 }
-
-fun logRepos(req: RequestData, response: Response<List<Repo>>) {
-    val repos = response.body()
-    if (!response.isSuccessful || repos == null) {
-        log.error("Failed loading repos for ${req.org} with response: '${response.code()}: ${response.message()}'")
-    }
-    else {
-        log.info("${req.org}: loaded ${repos.size} repos")
-    }
-}
-
-fun logUsers(repo: Repo, response: Response<List<User>>) {
-    val users = response.body()
-    if (!response.isSuccessful || users == null) {
-        log.error("Failed loading contributors for ${repo.name} with response '${response.code()}: ${response.message()}'")
-    }
-    else {
-        log.info("${repo.name}: loaded ${users.size} contributors")
-    }
-}
