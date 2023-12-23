591dc56dfa63fd123e1e23c8d607a245c5ac24bd
battlerhythm <16344965+battlerhythm@users.noreply.github.com>
2023-12-23 17:03:42 -0500

    Task1
    Signed-off-by: battlerhythm <16344965+battlerhythm@users.noreply.github.com>

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -3,8 +3,6 @@ package tasks
 import contributors.User
 
 /*
-TODO: Write aggregation code.
-
  In the initial list each user is present several times, once for each
  repository he or she contributed to.
  Merge duplications: each user should be present only once in the resulting list
@@ -15,4 +13,6 @@ TODO: Write aggregation code.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
 fun List<User>.aggregate(): List<User> =
-    this
+    groupBy { it.login }
+        .map { (login, group) -> User(login, group.sumOf { it.contributions }) }
+        .sortedByDescending { it.contributions }
